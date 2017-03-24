71d0e3dce5b653757c4bd3ff2ab9098ba2c98e49
GYCHEN <gy.chen@gms.nutc.edu.tw>
2017-03-24 13:49:38 +0800

    使用block wx:for顯示電子書圖片

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,5 +1,15 @@
 //index.js
 Page({
   data: {
+    pages_array: []
+  },
+  onLoad: function() {
+    let pages_array = [];
+    for(let i=0; i < 12; i++) {
+      pages_array.push(`/images/lfs-${i}.jpg`);
+    }
+    this.setData({
+      pages_array
+    });
   }
 })
