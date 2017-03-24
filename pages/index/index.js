804430fbc9ef335be37ba1406070be5a3808a147
GYCHEN <gy.chen@gms.nutc.edu.tw>
2017-03-24 14:04:02 +0800

    修復多圖片同時載入產生的效能與資源問題

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,12 +1,29 @@
 //index.js
+const PAGE_RANGE = 2;
+
 Page({
   data: {
-    pages_array: []
+    pages_array: [],
+    current_page: 0
   },
   onLoad: function() {
-    let pages_array = [];
-    for(let i=0; i < 12; i++) {
-      pages_array.push(`/images/lfs-${i}.jpg`);
+    this.calculate_pages_array();
+  },
+  on_swiper_change: function({ detail: { current: current_page } }) {
+    this.setData({
+      current_page
+    });
+    this.calculate_pages_array();
+  },
+  calculate_pages_array: function() {
+    const pages_array = [];
+    const { current_page } = this.data;
+    for(let i=0; i < 359; i++) {
+      let image_path = null;
+      if (current_page - PAGE_RANGE <= i && i <= current_page + PAGE_RANGE) {
+        image_path = `/images/lfs-${i}.jpg`;
+      }
+      pages_array.push(image_path);
     }
     this.setData({
       pages_array
